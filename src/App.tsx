72084647998a0828a40de3d4ae3ad4bed2e7fae4
Utkarshg25 <utkarsh.gangwar_cs19@gla.ac.in>
2021-10-08 23:56:27 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidebar name="Utkarsh" email="utkarshgangwar909@gmail.com" />
+    </div>
+  );
 }
 
 export default App;
