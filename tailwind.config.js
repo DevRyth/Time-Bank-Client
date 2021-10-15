44b251d6e9f4a593f53bd6482ff98ffecf2a56f9
shivam.yadav <shivam.yadav01@devslane.com>
2021-10-15 11:50:08 +0530

    added react icons and colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-   darkMode: false, // or 'media' or 'class'
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      color: {
+        primary: "#3C5654",
+        secondary: "#C89C32",
+        "primary-lite": "#425C5A",
+        "secondary-lite": "#D6AF14",
+        brandPrimary: "#3391b2",
+        brandSecondary: "#51B4A8",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
