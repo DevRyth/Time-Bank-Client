8ecd213b2dd5828ecb7cd6a0ebf06c31f746e63e
shivam.yadav <shivam.yadav01@devslane.com>
2021-10-19 00:51:02 +0530

    added formik in signup form

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Input from "../sharedComponents/input/Input";
 import { HiOutlineUser, MdEmail, CgPassword } from "react-icons/all";
 import Button from "../sharedComponents/button/Button";
 import SwitchButton from "../sharedComponents/switchButton/SwitchButton";
+import { Formik } from "formik";
 
 const SignUp: React.FC = () => {
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="bg-background-lite p-10 h-screen w-screen">
       <div className="text-center min-h-full p-10 bg-on-primary-lite">
@@ -18,48 +26,81 @@ const SignUp: React.FC = () => {
             Log in
           </Link>
         </p>
-        <div className="pt-6 space-y-3 md:space-y-6 md:pt-10">
-          <Input
-            iconColor="text-primary-dark"
-            className="text-primary-dark"
-            type="username"
-            name="text"
-            placeholder="Username"
-          >
-            <HiOutlineUser />
-          </Input>
-          <Input
-            iconColor="text-primary-dark"
-            className="text-primary-dark"
-            type="email"
-            name="email"
-            placeholder="Email"
-          >
-            <MdEmail />
-          </Input>
-          <Input
-            iconColor="text-primary-dark"
-            className="text-primary-dark"
-            type="password"
-            name="password"
-            placeholder="Password"
-          >
-            <CgPassword />
-          </Input>
-          <Input
-            iconColor="text-primary-dark"
-            className="text-primary-dark"
-            type="text"
-            name="text"
-            placeholder="Confirm Password"
-          >
-            <CgPassword />
-          </Input>
-          <SwitchButton className="pt-3" text="Show Password" />
-          <div className="pt-3">
-            <Button title="Sign Up" theme="primary" />
-          </div>
-        </div>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={async (values, helper) => {
+            const mappedValues = {
+              username: values.username,
+              email: values.email,
+              password: values.password,
+            };
+            console.log("Mapped Values", mappedValues);
+          }}
+        >
+          {(formikProps) => (
+            <form
+              onSubmit={formikProps.handleSubmit}
+              className="pt-6 space-y-3 md:space-y-6 md:pt-10"
+            >
+              <Input
+                onChange={formikProps.handleChange}
+                onBlur={formikProps.handleBlur}
+                value={formikProps.values.username}
+                iconColor="text-primary-dark"
+                className="text-primary-dark"
+                type="username"
+                name="text"
+                placeholder="Username"
+              >
+                <HiOutlineUser />
+              </Input>
+              <Input
+                onChange={formikProps.handleChange}
+                onBlur={formikProps.handleBlur}
+                value={formikProps.values.email}
+                iconColor="text-primary-dark"
+                className="text-primary-dark"
+                type="email"
+                name="email"
+                placeholder="Email"
+              >
+                <MdEmail />
+              </Input>
+              <Input
+                onChange={formikProps.handleChange}
+                onBlur={formikProps.handleBlur}
+                value={formikProps.values.password}
+                iconColor="text-primary-dark"
+                className="text-primary-dark"
+                type="password"
+                name="password"
+                placeholder="Password"
+              >
+                <CgPassword />
+              </Input>
+              <Input
+                onChange={formikProps.handleChange}
+                onBlur={formikProps.handleBlur}
+                value={formikProps.values.confirmPassword}
+                iconColor="text-primary-dark"
+                className="text-primary-dark"
+                type={showPassword ? "text" : "password"}
+                placeholder="Confirm Password"
+              >
+                <CgPassword />
+              </Input>
+              <SwitchButton
+                defaultChecked={showPassword}
+                onChange={() => setShowPassword(!showPassword)}
+                className="pt-3"
+                text="Show Password"
+              />
+              <div className="pt-3">
+                <Button title="Sign Up" theme="primary" />
+              </div>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
