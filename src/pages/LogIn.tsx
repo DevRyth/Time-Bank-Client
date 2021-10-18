2ae2c7db2d69da90060e82c5562e795f293702da
akarshgangwar28 <akarshprog28@gmail.com>
2021-10-19 00:25:09 +0530

    Created Login page

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../sharedComponents/input/Input";
-import { HiOutlineUser, MdEmail, CgPassword } from "react-icons/all";
+import { HiOutlineUser,  CgPassword } from "react-icons/all";
 import Button from "../sharedComponents/button/Button";
 
-const SignUp: React.FC = () => {
+const LogIn: React.FC = () => {
   return (
     <div className="bg-background-lite p-10 h-screen w-screen">
       <div className="text-center min-h-full p-10 bg-on-primary-lite">
         <h1 className="text-center text-xl md:text-3xl font-bold">
-          Get Started
+        Welcome
         </h1>
         <p className="tracking-wider md:text-sm pt-4 text-xs">
-          Already have an account?{" "}
-          <Link className="text-secondary-dark underline" to="/login">
-            Log in
+          Did'nt have an account?{" "}
+          <Link className="text-secondary-dark underline" to="/signup">
+            Create One
           </Link>
         </p>
         <div className="pt-6 space-y-3 md:space-y-6 md:pt-10">
@@ -23,19 +23,11 @@ const SignUp: React.FC = () => {
             className="text-primary-dark"
             type="username"
             name="text"
-            placeholder="Username"
+            placeholder="Username or Email"
           >
             <HiOutlineUser />
           </Input>
-          <Input
-            iconColor="text-primary-dark"
-            className="text-primary-dark"
-            type="email"
-            name="email"
-            placeholder="Email"
-          >
-            <MdEmail />
-          </Input>
+        
           <Input
             iconColor="text-primary-dark"
             className="text-primary-dark"
@@ -45,17 +37,9 @@ const SignUp: React.FC = () => {
           >
             <CgPassword />
           </Input>
-          <Input
-            iconColor="text-primary-dark"
-            className="text-primary-dark"
-            type="text"
-            name="text"
-            placeholder="Confirm Password"
-          >
-            <CgPassword />
-          </Input>
+    
           <div className="pt-5">
-            <Button title="Sign Up" theme="primary" />
+            <Button title="Log In" theme="primary" />
           </div>
         </div>
       </div>
@@ -63,4 +47,4 @@ const SignUp: React.FC = () => {
   );
 };
 
-export default React.memo(SignUp);
+export default React.memo(LogIn);
