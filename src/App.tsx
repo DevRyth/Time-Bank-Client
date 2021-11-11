a75b0b41637fea74d5b11de62f9339ef2e37b094
divyanshu9307 <divyanshu9307@gmail.com>
2021-11-12 00:20:37 +0530

    done making status card

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import SignUp from "./pages/SignUp";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import LogIn from "./pages/LogIn";
+import CardTimeStatus from "./sharedComponents/cardTimeStatus/CardTimeStatus";
 
 function App() {
   return (
     <div>
-
       <Router>
         <Switch>
           <Route exact path="/dashboard">
@@ -23,10 +23,21 @@ function App() {
             <SignUp />
           </Route>
           <Route exact path="/login">
-            <LogIn/>
+            <LogIn />
+          </Route>
+          <Route exact path="/card">
+            <CardTimeStatus
+              name="ABCD"
+              className="bg-primary-lite"
+              num={24244}
+            />
+            <CardTimeStatus
+              name="Estimated time"
+              className="bg-secondary-lite"
+              num={15455}
+            />
           </Route>
         </Switch>
-
       </Router>
     </div>
   );
---
+++ src/sharedComponents/cardTimeStatus/CardTimeStatus.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+interface Props {
+  name: string;
+  num: number;
+  className?: string;
+}
+const CardTimeStatus: React.FC<Props> = ({ name, className, num }) => {
+  return (
+    <div
+      className={`h-32 w-72 text-center text-white rounded-3xl ${className}`}
+    >
+      <div className="text-white">{name}</div>
+      <div className="text-white font-bold text-2xl p-4">{num}</div>
+    </div>
+  );
+};
+export default CardTimeStatus;
