be9f6ac0751657d1d498eff57fc3dfb276f2cc6f
shivamy2 <sy7052789@gmail.com>
2021-11-12 22:16:19 +0530

    completed courses page

--- src/sharedComponents/Cards/CourseCards.tsx
+++ src/sharedComponents/Cards/CourseCards.tsx
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import CourseDefaultImage from "../../Images/coursedefault.jpg";
-import { MdFavoriteBorder, BiTimeFive } from "react-icons/all";
+import { MdFavoriteBorder, BiTimeFive, MdFavorite } from "react-icons/all";
 
-const CourseCards: React.FC = () => {
-  const fields = [
-    {
-      teacherName: "Shivam Yadav",
-      title: "Course 1",
-      duration: "00:40:00",
-    },
-  ];
+interface Props {
+  data: { "teacher-name": string; title: string; duration: string };
+}
+
+const CourseCards: React.FC<Props> = ({ data }) => {
+  const [isFavouriteClicked, setIsFavouriteClicked] = useState(false);
+  const slicedTitle = data.title.substring(0, 40);
+
   return (
     <div className="mx-auto md:mx-0">
-      <div className="w-72 h-80 mx-auto">
+      <div className="w-72 h-80 cursor-pointer rounded-tl-lg mx-auto shadow-stacked hover:scale-95 transform ease-in-out transition-all">
         <div className="bg-gray-300 h-1/2">
           <img
             src={CourseDefaultImage}
-            className="h-full w-full rounded-tl-lg shadow-primary object-cover object-center"
+            className="h-full w-full object-cover object-center"
             alt="courseImage"
           />
         </div>
         <div className="rounded-br-lg px-6 pt-3 bg-white h-1/2">
-          <div className="">{fields[0].teacherName}</div>
-          <div className="font-extrabold mt-2 text-lg">{fields[0].title}</div>
-          <div className="flex justify-between">
+          <div className="">{data["teacher-name"]}</div>
+          <div className="font-extrabold mt-2 text-lg h-16">{slicedTitle}</div>
+          <div className="flex justify-between ">
             <div className="flex">
-              <MdFavoriteBorder className="text-primary-dark h-10 my-auto" />
+              {isFavouriteClicked ? (
+                <MdFavorite
+                  onClick={() => setIsFavouriteClicked(false)}
+                  className="text-red-600 h-5 w-5 my-auto"
+                />
+              ) : (
+                <MdFavoriteBorder
+                  onClick={() => setIsFavouriteClicked(true)}
+                  className="text-primary-dark h-5 w-5 my-auto"
+                />
+              )}
             </div>
             <div className="flex pr-5">
               <BiTimeFive className="my-auto mr-3 text-primary-dark h-10" />
-              <span className="my-auto">{fields[0].duration}</span>
+              <span className="my-auto">{data.duration}</span>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         primary: "0 10px 15px -3px #ffffff, 0 4px 6px -2px rgba(0, 0, 0, 0)",
         warning: "0 10px 15px -3px #F6E1C2, 0 4px 6px -2px rgba(0, 0, 0, 0)",
         progress: "rgb(241, 242, 243) 1px 3px 20px 3px",
-        stacked: "0px 0px 15px 1px rgb(113 106 202 / 10%)",
+        stacked: "0px 0px 45px 10px rgb(113 106 202 / 15%)",
         "more-button": "rgb(208, 208, 208) 0px 0px 20px 0px",
         "sidebar-elements":
           "rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px",
