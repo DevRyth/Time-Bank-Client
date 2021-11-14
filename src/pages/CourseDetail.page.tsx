0d0b82e0a7274fcaaf3e1600d6b4e6d60c9a25d9
shivamy2 <sy7052789@gmail.com>
2021-11-14 21:33:12 +0530

    added about us section

--- src/pages/CourseDetail.page.tsx
+++ src/pages/CourseDetail.page.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import CourseImage from "../Images/coursedefault.jpg";
 import Button from "../sharedComponents/button/Button";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/all";
 
 const CourseDetail: React.FC = () => {
+  const [isAboutSliced, setIsAboutSliced] = useState(true);
   const field = {
     image: CourseImage,
     "teacher-name": "Shivam Yadav",
@@ -12,7 +14,15 @@ const CourseDetail: React.FC = () => {
     comments: 2,
     duration: "00:30:00",
     about:
-      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis placeat vero velit ratione iusto debitis a voluptates fuga similique architecto, obcaecati esse deleniti provident nemo impedit, veniam dolor ut? Voluptates!",
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis placeat vero velit ratione iusto debitis a voluptates fuga similique architecto, obcaecati esse deleniti provident nemo impedit, veniam dolor ut? Voluptates! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis placeat vero velit ratione iusto debitis a voluptates fuga similique architecto, obcaecati esse deleniti provident nemo impedit, veniam dolor ut? Voluptates!",
+  };
+
+  const slicedAbout = isAboutSliced
+    ? field.about.substring(0, 200)
+    : field.about;
+
+  const handleSliceClicked = () => {
+    setIsAboutSliced(!slicedAbout);
   };
 
   return (
@@ -22,7 +32,7 @@ const CourseDetail: React.FC = () => {
           <img
             src={field.image}
             alt="courseimage"
-            className="w-2/3 rounded-xl border-2 border-primary-dark shadow-stacked mx-auto object-cover h-full object-center"
+            className="w-44 rounded-xl border-1 border-primary-dark shadow-stacked mx-auto object-cover h-full object-center"
           />
         </div>
         <div className="md:col-span-2 md:my-auto text-center md:text-left">
@@ -54,6 +64,28 @@ const CourseDetail: React.FC = () => {
               </p>
             </div>
           </div>
+        </div>
+        <hr className="border mt-3 md:hidden" />
+      </div>
+      <div className="m-3 rounded-xl p-6 shadow-stacked">
+        <div>
+          <h2 className="text-lg font-bold">About this course</h2>
+        </div>
+        <div className="mt-3">
+          <p className="text-sm">{slicedAbout}</p>
+        </div>
+        <div className="flex mt-3 cursor-pointer" onClick={handleSliceClicked}>
+          {isAboutSliced ? (
+            <>
+              <AiOutlinePlusCircle className="my-auto text-primary-dark" />
+              <span className="text-sm ml-1 text-primary-dark">See more</span>
+            </>
+          ) : (
+            <>
+              <AiOutlineMinusCircle className="my-auto text-primary-dark" />
+              <span className="text-sm ml-1 text-primary-dark">See less</span>
+            </>
+          )}
         </div>
       </div>
     </div>
