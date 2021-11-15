7b6d5e1cac5e55585273427377776f9affbd7a35
akarshgangwar28 <akarshprog28@gmail.com>
2021-11-15 23:49:24 +0530

    Appointment page updated

--- src/components/Appointment.component.tsx
+++ src/components/Appointment.component.tsx
@@ -4,13 +4,45 @@ import Input from '../sharedComponents/input/Input'
 
 
 export default function Appointment() {
+    const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const min=[];
+    for(let i=1;i<=59;i++)
+    min.push(i);
     return (
         <div>
             <div className="border border-gray-900 w-1/3 	 ">
                 <div className="p-1"><AiOutlinePlusCircle/></div> <h1 className="text-center font-bold">Appointment Details</h1>
                 <div className="flex space-x-5 justify-center " >
-                <label>Date</label>
-                <Input type="date" children={undefined} name="appointment-date"  ></Input>
+                <select name="days">
+                    <option selected>Days</option>
+                    {
+                        days.map(item => (
+                            <option >{item}</option>
+                        ))
+                    }
+                </select>
+                <select name="min">
+                <option selected>min</option>
+                {
+                    min.map(item =>(
+                        <option>{item}</option>
+                    ))
+
+                }
+                 
+                 </select>
+
+                 <select name="sec">
+                <option selected>sec</option>
+                {
+                    min.map(item =>(
+                        <option>{item}</option>
+                    ))
+
+                }
+                 
+                 </select>
+
                 </div>
                 </div>
         </div>
