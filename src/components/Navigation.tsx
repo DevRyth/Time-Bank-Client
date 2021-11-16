26ac3aef70bf939a5d9973dc632b9b9f91e248f9
Utkarshg25 <utkarsh.gangwar_cs19@gla.ac.in>
2021-11-16 10:05:24 +0530

    made some new changes in Avatar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -16,7 +16,7 @@ const Navigation: React.FC<Props> = () => {
             <div className=" flex justify-end mr-2 py-1">
               <a href="#">
                 <Avatar
-                  shape="rounded-full"
+                  shape="rounded"
                   URL="https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk="
                 />
               </a>
--- src/sharedComponents/Avatar.tsx
+++ src/sharedComponents/Avatar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 interface Props {
-  shape: String;
+  shape: "circular" | "rounded";
   URL: String;
 }
 const Avatar: React.FC<Props> = ({ shape, URL }) => {
-  return <img src={`${URL}`} alt="" className={`${shape} h-10 w-10`} />;
+  const shapeClass = shape === "circular" ? "rounded-full" : "rounded-none";
+  return <img src={`${URL}`} alt="" className={`${shapeClass} h-10 w-10`} />;
 };
 export default Avatar;
