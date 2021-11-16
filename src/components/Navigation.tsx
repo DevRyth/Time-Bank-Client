5e9bd0d2db691aa29eff5454df433c134ec0dbd4
Utkarshg25 <utkarsh.gangwar_cs19@gla.ac.in>
2021-11-16 09:52:25 +0530

    update the Avatar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,7 +15,10 @@ const Navigation: React.FC<Props> = () => {
           <nav className="">
             <div className=" flex justify-end mr-2 py-1">
               <a href="#">
-                <Avatar shape="rounded-full" />
+                <Avatar
+                  shape="rounded-full"
+                  URL="https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk="
+                />
               </a>
             </div>
           </nav>
