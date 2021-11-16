656657e0861b22dc5f843d794f6c0310aeff3449
Utkarshg25 <utkarsh.gangwar_cs19@gla.ac.in>
2021-11-16 14:50:12 +0530

    ...

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,12 +14,12 @@ const Navigation: React.FC<Props> = () => {
         <header className="bg-on-secondary ">
           <nav className="">
             <div className=" flex justify-end mr-2 py-1">
-              <a href="#">
+              <div onClick={update}>
                 <Avatar
                   shape="rounded"
                   URL="https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1057506940?k=20&m=1057506940&s=612x612&w=0&h=3j5EA6YFVg3q-laNqTGtLxfCKVR3_o6gcVZZseNaWGk="
                 />
-              </a>
+              </div>
             </div>
           </nav>
         </header>
