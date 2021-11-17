817bd369f0bb5e8ea2a508b80f41fe4abc99daf6
akarshgangwar28 <akarshprog28@gmail.com>
2021-11-17 21:37:32 +0530

    Appointment details page

--- src/components/Appointment.component.tsx
+++ src/components/Appointment.component.tsx
@@ -1,50 +1,47 @@
-import React from 'react'
-import { AiOutlinePlusCircle } from 'react-icons/ai'
-import Input from '../sharedComponents/input/Input'
-
+import React from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 export default function Appointment() {
-    const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const min=[];
-    for(let i=1;i<=59;i++)
-    min.push(i);
-    return (
-        <div>
-            <div className="border border-gray-900 w-1/3 	 ">
-                <div className="p-1"><AiOutlinePlusCircle/></div> <h1 className="text-center font-bold">Appointment Details</h1>
-                <div className="flex space-x-5 justify-center " >
-                <select name="days">
-                    <option selected>Days</option>
-                    {
-                        days.map(item => (
-                            <option >{item}</option>
-                        ))
-                    }
-                </select>
-                <select name="min">
-                <option selected>min</option>
-                {
-                    min.map(item =>(
-                        <option>{item}</option>
-                    ))
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const min = [];
+  for (let i = 1; i <= 59; i++) min.push(i);
+  return (
+    <div>
+      <div className="border border-gray-900  	 p-3 ">
+        <button className="p-1">
+          <AiOutlinePlusCircle />
+        </button>
+        <h1 className="text-center font-bold ">Appointment Details</h1>
+        <div className="flex space-x-5 justify-center ">
+          <select name="days" className="h-6	">
+            <option selected>Days</option>
+            {days.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
+          <select className="h-5" name="min">
+            <option selected>min</option>
+            {min.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
 
-                }
-                 
-                 </select>
-
-                 <select name="sec">
-                <option selected>sec</option>
-                {
-                    min.map(item =>(
-                        <option>{item}</option>
-                    ))
-
-                }
-                 
-                 </select>
-
-                </div>
-                </div>
+          <select name="sec">
+            <option selected>sec</option>
+            {min.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
