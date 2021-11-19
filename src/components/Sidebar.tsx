fe3c9e168d4766cd489d86624617839f9fac52e3
Utkarshg25 <utkarsh.gangwar_cs19@gla.ac.in>
2021-11-20 00:55:55 +0530

    reuse ListElement component in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { FaFolder } from "react-icons/fa";
+import { IoIosStats } from "react-icons/io";
 import { Link } from "react-router-dom";
 import Avatar from "../sharedComponents/Avatar";
+import ListElement from "../sharedComponents/ListElement";
 
 interface Props {
   name: string;
@@ -23,51 +27,30 @@ const Sidebar: React.FC<Props> = ({ name, email }) => {
         </h3>
       </div>
       <div className="mt-4">
-        <li className="flex hover:bg-primary-lite cursor-pointer list-none border-b p-2 text-2xl font-semibold text-center tracking-wider border-gray-600">
-          <Link to="/dashboard" className="flex text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 text-white my-auto mr-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
-            <span className="text-base">Dashboard</span>
-          </Link>
-        </li>
-        <li className="list-none hover:bg-primary-lite cursor-pointer border-b p-2 text-2xl font-semibold text-center tracking-wider border-gray-600">
-          <Link to="/statics" className="flex text-white text-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mr-4 my-auto"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
-            </svg>
-            <span className="text-base">Statistics</span>
-          </Link>
-        </li>
-        <li className="list-none hover:bg-primary-lite border-b p-2 text-2xl font-semibold text-center tracking-wider border-gray-600">
-          <Link to="/courses" className="flex text-white text-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mr-4 my-auto"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
-            </svg>
-            <span className="text-base">Courses</span>
-          </Link>
-        </li>
+        <ListElement
+          title="Dashboard"
+          path="/dashboard"
+          icon={<AiOutlineHome className="text-white my-auto " />}
+          className={
+            "list-none hover:bg-primary-lite cursor-pointer border-b p-2 text-base text-white font-semibold text-center tracking-wider border-gray-600"
+          }
+        />
+        <ListElement
+          title="Statistics"
+          path="/statics"
+          icon={<IoIosStats className="text-white my-auto " />}
+          className={
+            "list-none hover:bg-primary-lite cursor-pointer border-b p-2 text-base text-white font-semibold text-center tracking-wider border-gray-600"
+          }
+        />
+        <ListElement
+          title="Courses"
+          path="/course"
+          icon={<FaFolder className="text-white my-auto " />}
+          className={
+            "list-none hover:bg-primary-lite cursor-pointer border-b p-2 text-base text-white font-semibold text-center tracking-wider border-gray-600"
+          }
+        />
         <li
           className="list-none border-b hover:bg-primary-lite p-2 text-2xl font-semibold text-center tracking-wider border-gray-600"
           onClick={() => {
--- src/sharedComponents/ListElement.tsx
+++ src/sharedComponents/ListElement.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Link } from "react-router-dom";
 interface Props {
   title: string;
   path?: string;
   className?: string;
-  icon?: string;
+  icon?: ReactNode;
 }
-const ListElement: React.FC<Props> = ({ title, className, path }) => {
+const ListElement: React.FC<Props> = ({ title, className, path, icon }) => {
   return (
     <Link to={path!}>
-      <li className={`list-none px-2  ${className}`}>{title}</li>
+      <li className={`list-none px-2 flex ${className}`}>
+        <div className="flex-row m-2 my-auto">{icon}</div>
+        <span>{title}</span>
+      </li>
     </Link>
   );
 };
 
-export default ListElement;
+export default React.memo(ListElement);
