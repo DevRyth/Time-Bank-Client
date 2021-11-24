515dc734714892017ec6534efd8212f8047ad31a
shivamy2 <sy7052789@gmail.com>
2021-11-25 01:36:04 +0530

    updated about-us

--- src/components/aboutus/Aboutus.tsx
+++ src/components/aboutus/Aboutus.tsx
@@ -2,15 +2,14 @@ import React from "react";
 interface Props {}
 const Aboutus: React.FC<Props> = () => {
   return (
-    <div className="h-screen bg-primary-dark">
-      <div className="h-1/3  border-2 border-secondary-lite text-center pt-20 font-extrabold text-secondary-dark text-4xl">
-        {" "}
+    <div className="h-screen">
+      <div className="h-1/3 border-2 border-secondary-lite text-center pt-20 font-extrabold text-primary-dark text-3xl">
         Our Company
       </div>
-      <div className="h-full  bg-secondary-dark">
-        <div className=" m-4  mt-10 min-h-screen bg-primary-dark text-4xl text-center text-secondary-dark flex flex-col lg:mt-20">
+      <div className="h-full">
+        <div className="m-4 mt-10 min-h-screen text-3xl font-extrabold text-center text-primary-dark flex flex-col lg:mt-20">
           You Teach You Learn
-          <div className=" mt-4 mb-8 text-center text-xl lg:mt-10">
+          <div className=" mt-4 mb-8 text-center text-sm lg:mt-10 text-secondary-dark">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat
             nihil harum natus, esse cum saepe ducimus libero repellendus veniam
             reprehenderit aperiam dolorem incidunt quos, illo tempora omnis!
@@ -19,12 +18,14 @@ const Aboutus: React.FC<Props> = () => {
             maiores ipsum expedita deserunt, repellat quibusdam ipsam molestiae
             possimus sed? Magni, ducimus! Natus dolorem quasi rem!
           </div>
-          <div className="mt-20 text-center text-2xl">Members</div>
-          <div className="text-center text-sm">Shivam yadav</div>
-          <div className="text-center text-sm">Abhishek kumar singh</div>
-          <div className="text-center text-sm">Utkarsh gangwar</div>
-          <div className="text-center text-sm">Akarsh gangwar</div>
-          <div className="text-center text-sm">Divyanshu sinha</div>
+          <div className="mt-20 text-center text-3xl">Members</div>
+          <div className="text-lg text-center mt-6 text-secondary-dark">
+            <div className="">Shivam Yadav</div>
+            <div className="">Abhishek Kumar Singh</div>
+            <div className="">Utkarsh Gangwar</div>
+            <div className="">Akarsh Gangwar</div>
+            <div className="">Divyanshu Sinha</div>
+          </div>
         </div>
       </div>
     </div>
